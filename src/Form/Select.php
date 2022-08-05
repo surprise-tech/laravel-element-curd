ee7213a4a99d7f78d2e4d00eceacd9accb349b46
wyzheng1997 <wyzheng1997@163.com>
2022-08-05 08:33:44 +0800

    fix: 修复select选型为number时不能被选中的bug

--- src/Form/Select.php
+++ src/Form/Select.php
@@ -32,7 +32,7 @@ class Select extends FormItem
         $opts = [];
         foreach ($options as $key => $val) {
             $temp = [
-                'value' => $key,
+                'value' => (string) $key,
                 'label' => $val,
             ];
             if ($callback) {
